11727c0a8f58b57962c72f1b66961cd7e28de545
shinyatlas <mannyoso07@gmail.com>
2026-04-23 11:37:49 -0400

    add Order class and OrderType enum for order management

---
+++ Order.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <array>
+
+// all special "types" needed for an order
+enum class OrderType {
+    BUY,
+    SELL,
+    HOLD
+};
+
+// the order itself, a structure holding all relevant information
+// and the main object that the program will interact with.
+struct Order {
+    OrderType type;
+    int id;
+    int price;
+};
