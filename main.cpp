4609adb17a5e8778b2a26d0ec39d7f13bf3a1a9c
shinyatlas <mannyoso07@gmail.com>
2026-04-23 11:38:30 -0400

    add main functionality to simulate order book operations

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include "Order.hpp"
+#include "OrderBook.hpp"
+
+using namespace std;
+
+void addHistory(Order& order, vector<Order>& history);
+
+int main() {
+    //create an order book object
+    OrderBook orderBook;
+
+    while (true) {
+        //get user input for order details
+        int id, price;
+        string type;
+        cout << "Enter order type (BUY/SELL/HOLD), id, and price: ";
+        cin >> type >> id >> price;
+
+        //create an order object based on user input
+        OrderType orderType;
+        if (type == "BUY") {
+            orderType = OrderType::BUY;
+        } else if (type == "SELL") {
+            orderType = OrderType::SELL;
+        } else {
+            orderType = OrderType::HOLD;
+        }
+        Order newOrder{orderType, id, price};
+
+        //add the order to the order book
+        orderBook.addOrder(newOrder);
+
+        //match orders in the order book
+        orderBook.matchOrders();
+    }
+}
+
+void addHistory(Order& order, vector<Order>& history) {
+    history.push_back(order);
+}
