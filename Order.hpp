ea400ecfa88649cadcc92fa44a8b306abfe2c754
shinyatlas <mannyoso07@gmail.com>
2026-06-11 21:38:23 -0400

    added order direction, status, and type information

--- Order.hpp
+++ Order.hpp
@@ -1,18 +1,33 @@
 #pragma once
 #include <array>
+#include <cstdint>
 
 // all special "types" needed for an order
-enum class OrderType {
+enum class OrderDirection {
     BUY,
-    SELL,
-    HOLD
+    SELL
+};
+
+enum class OrderStatus {
+    OPEN,
+    PARTIALLY_FILLED,
+    FILLED,
+    CANCELLED
+};
+
+enum OrderType {
+    LIMIT,
+    MARKET
 };
 
 // the order itself, a structure holding all relevant information
 // and the main object that the program will interact with.
 struct Order {
     OrderType type;
+    OrderDirection direction;
     int id;
     int price;
     int quantity;
+    uint64_t timestamp; // Time in nanoseconds since order creation, used for time priority in matching
+    OrderStatus status = OrderStatus::OPEN; // Default status is OPEN
 };
