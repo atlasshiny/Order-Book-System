f43c2dbb810234f3931394659015b6caea6182d5
shinyatlas <mannyoso07@gmail.com>
2026-06-15 17:01:21 -0400

    fixed method definition build error

--- include/risk/SimpleRiskManager.hpp
+++ include/risk/SimpleRiskManager.hpp
@@ -6,5 +6,5 @@ private:
     const int MAX_ORDER_QUANTITY = 1000;
     const int ORDER_PER_SECOND_LIMIT = 100; // When ASIO is implemented, ensure that the number of orders per second does not exceed this limit
 public:
-    bool checkOrder(const Order& order) override {};
+    bool checkOrder(const Order& order) override;
 };
