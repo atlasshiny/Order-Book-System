13f2b92ff3730f37e5f8aae0184f4cc847d9d3fe
ShinyAtlas <56489130+atlasshiny@users.noreply.github.com>
2026-06-17 20:19:45 -0400

    added comments and some header tags

--- include/parsers/FIXDefinition.hpp
+++ include/parsers/FIXDefinition.hpp
@@ -4,7 +4,13 @@
 namespace FIX {
     // Standard FIX tags for order fields
     namespace Tags {
+    // header tags for the messages
+        constexpr int BeginString = 8;
+        constexpr int BodyLength = 9;
         constexpr int MsgType = 35;
+
+        // common order fields
+        constexpr int Symbol = 55;
         constexpr int SIDE = 54;
         constexpr int PRICE = 44;
         constexpr int QUANTITY = 38;
@@ -14,7 +20,6 @@ namespace FIX {
 
     namespace MsgTypes {
         constexpr char NewOrderSingle = 'D';
-        constexpr std::string_view ExecutionReport = "8";
         // Add other message types as needed
     }
 }
