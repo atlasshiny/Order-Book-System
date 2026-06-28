12f155a0ca7404a47b27cea842baff42b9538e8e
shinyatlas <mannyoso07@gmail.com>
2026-06-27 23:18:40 -0400

    changed gateway interface to better reflect what ASIO expects

--- include/gateways/IGateway.hpp
+++ include/gateways/IGateway.hpp
@@ -2,12 +2,20 @@
 #include "orderbook/Order.hpp"
 #include <optional>
 #include <string_view>
-
+#include "network/TCPSession.hpp"
 class IGateway {
     public:
         virtual ~IGateway() = default;
+
+        // Called by TCPSession when raw bytes arrive over the wire
+        virtual void on_data_received(std::shared_ptr<TCPSession> session, std::string_view raw_data) = 0;
+
+        // Called by TCPSession when a connection drops or initiates
+        virtual void on_client_connect(std::shared_ptr<TCPSession> session) = 0;
+        virtual void on_client_disconnect(std::shared_ptr<TCPSession> session) = 0;
+
+    private:
         virtual size_t sendOrder(const Order& order, char* wireBuffer_, size_t bufferSize) = 0;
         virtual std::optional<Order> receiveOrder(std::string_view rawData) = 0;
         virtual void cancelOrder(int orderId) = 0;
-        virtual void onMarketDataUpdate() = 0;
 };
