e93aa6b56ba1134f4cef371b6e734d4c75b56427
shinyatlas <mannyoso07@gmail.com>
2026-06-29 02:26:12 -0400

    added writeExecutionReport to the writer interface

--- include/writers/FIXWriter.hpp
+++ include/writers/FIXWriter.hpp
@@ -6,5 +6,5 @@ class FIXWriter : public IWriter {
     public:
         size_t write(const Order& order, char* buffer, size_t bufferSize) override;
 
-        size_t writeExecutionReport(const Order& order, std::string_view execType, char* buffer, size_t bufferSize);
+        size_t writeExecutionReport(const Order& order, std::string_view execType, char* buffer, size_t bufferSize) override;
 };
--- include/writers/IWriter.hpp
+++ include/writers/IWriter.hpp
@@ -5,5 +5,8 @@
 class IWriter {
     public:
         virtual size_t write(const Order& order, char* buffer, size_t bufferSize) = 0;
+
+        virtual size_t writeExecutionReport(const Order& order, std::string_view execType, char* buffer, size_t bufferSize) = 0;
+
         virtual ~IWriter() = default;
 };
