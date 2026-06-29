8fe12fc771fe49bf32243557840ecd1a235d310b
shinyatlas <mannyoso07@gmail.com>
2026-06-28 20:19:47 -0400

    added ExecType and OrdStatus to the execution report

--- include/writers/FIXWriter.hpp
+++ include/writers/FIXWriter.hpp
@@ -1,9 +1,10 @@
 #pragma once
 #include "IWriter.hpp"
+#include <iostream>
 
 class FIXWriter : public IWriter {
     public:
         size_t write(const Order& order, char* buffer, size_t bufferSize) override;
 
-        size_t writeExecutionReport(const Order& order, std::string_view execType, std::string_view ordStatus, char* buffer, size_t bufferSize);
+        size_t writeExecutionReport(const Order& order, std::string_view execType, char* buffer, size_t bufferSize);
 };
