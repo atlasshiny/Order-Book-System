f302b7f41a4575e721685bb526c734f53f79bc46
shinyatlas <mannyoso07@gmail.com>
2026-06-29 22:41:45 -0400

    updated writeExecutionReport parameters to have lastShares

--- include/writers/FIXWriter.hpp
+++ include/writers/FIXWriter.hpp
@@ -5,5 +5,5 @@ class FIXWriter : public IWriter {
     public:
         size_t write(const Order& order, char* buffer, size_t bufferSize) override;
 
-        size_t writeExecutionReport(const Order& order, std::string_view execType, std::string_view ordStatus, char* buffer, size_t bufferSize) override;
+        size_t writeExecutionReport(const Order& order, int lastShares, std::string_view execType, std::string_view ordStatus, char* buffer, size_t bufferSize) override;
 };
